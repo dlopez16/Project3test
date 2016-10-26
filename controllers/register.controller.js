a845602af47b945f1aa4693f0fce75688fd2113b
Daniel Lopez <Daniel Lopez>
2016-10-25 21:03:48 -0400

    lo2l

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res) {
     // register using api to maintain clean separation between layers
     request.post({
-        url: 'https://127.0.0.1:' + process.env.PORT + '/users/register',
+        url: 'https://dashboard.heroku.com/apps/lit-escarpment-80522' + process.env.PORT + '/users/register',
         form: req.body,
         json: true
     }, function (error, response, body) {
