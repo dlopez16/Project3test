8edd5d29847fb2c164bcd4911774f6615b083669
Daniel Lopez <Daniel Lopez>
2016-10-25 20:28:14 -0400

    update6

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res) {
     // register using api to maintain clean separation between layers
     request.post({
-        url: localhost:3000 || localhost:process.env.PORT + '/users/register',
+        url: localhost:3000 || process.env.PORT + '/users/register',
         form: req.body,
         json: true
     }, function (error, response, body) {
