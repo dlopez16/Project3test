d433535a982d2649aa9c51e40fbb8c884649a9b9
Daniel Lopez <Daniel Lopez>
2016-10-26 17:00:06 -0400

    update24

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var expressJwt = require('express-jwt');
 var config = require('config.json');
 var mongoose = require('mongoose');
 //var mongodb = require('mongodb');
+var path = require('path');
 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/test');
@@ -21,8 +22,9 @@ app.use(session({ secret: config.secret, resave: false, saveUninitialized: true 
 // use JWT auth to secure the api
 app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
 
-// routes
+// 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use('/login', require('./controllers/login.controller'));
 app.use('/register', require('./controllers/register.controller'));
 app.use('/app', require('./controllers/app.controller'));
